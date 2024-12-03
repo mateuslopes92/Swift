871fba1697613882d3971423be0123e96203dfa7
mateuslopes92 <mateuslopes92@gmai.com>
2024-12-02 22:03:07 -0300

    added enums and treat for status code

--- SwiftApps/Habit/Habit/Common/Network/WebService.swift
+++ SwiftApps/Habit/Habit/Common/Network/WebService.swift
@@ -12,16 +12,28 @@ enum WebService {
         case postUser = "/users"
     }
     
+    enum NetworkError: Error {
+        case badRequest
+        case notFound
+        case unauthorized
+        case internalServerError
+    }
+    
+    enum Result {
+        case success(Data)
+        case failure(NetworkError, Data?)
+    }
+    
     private static func completeUrl(path: Endpoint) -> URLRequest? {
         guard let url = URL(string: "\(Endpoint.base.rawValue)\(path.rawValue)") else { return nil }
         return URLRequest(url: url)
     }
     
-    static func postUser(request: SignUpRequest)
-    {
-        guard let jsonData = try? JSONEncoder().encode(request) else { return }
-        
+    private static func call<T: Encodable>(path: Endpoint, body: T, completion: @escaping (Result) -> Void) {
         guard var urlRequest = completeUrl(path: .postUser) else { return }
+        
+        guard let jsonData = try? JSONEncoder().encode(body) else { return }
+
         
         urlRequest.httpMethod = "POST"
         urlRequest.setValue("application/json",forHTTPHeaderField: "accept")
@@ -31,18 +43,43 @@ enum WebService {
         let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
             guard let data = data, error == nil else {
                 print("error: \(error?.localizedDescription ?? "Unknown error")")
+                completion(.failure(.internalServerError, nil))
                 return
             }
             
-            print(String(data: data, encoding: .utf8))
+            if let r = response as? HTTPURLResponse {
+                switch r.statusCode {
+                case 400:
+                    completion(.failure(.badRequest, data))
+                case 200:
+                    completion(.success(data))
+                default:
+                    break
+                }
+            }
+            
+            
             
             print("response\n")
             print(response)
            
-            if let r = response as? HTTPURLResponse {
-                print("response: \(r.statusCode)")
-            }
+
         }
         task.resume()
+    }
+    
+    static func postUser(request: SignUpRequest){
+        call(path: .postUser, body: request){ result in
+            switch result {
+            case .success(let data):
+                print("User created successfully")
+                print(String(data: data, encoding: .utf8))
+            case .failure(let error, let data):
+                if let data = data {
+                    print("Error creating user")
+                    print(String(data: data, encoding: .utf8))
+                }
+            }
+        }
     }
 }
