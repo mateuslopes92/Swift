5e4d91bb82fb6daa2cf47ce15ccf3728cb58e53a
mateuslopes92 <mateuslopes92@gmail.com>
2025-11-10 23:21:49 -0300

    Added navigation

--- SwiftApps/iChat/iChat/SignInView.swift
+++ SwiftApps/iChat/iChat/SignInView.swift
@@ -9,71 +9,71 @@ import SwiftUI
 
 struct SignInView: View {
     @StateObject var viewModel: SignInViewModel = SignInViewModel()
-
+    
     var body: some View {
-        VStack {
-            Image("chat_logo")
-                .resizable()
-                .scaledToFit()
-                .padding()
-            
-            TextField("E-mail", text: $viewModel.email)
-                .autocapitalization(.none)
-                .disableAutocorrection(false)
-                .padding()
-                .background(Color.white)
-                .cornerRadius(24.0)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 24.0)
-                        .strokeBorder(
-                            Color(UIColor.separator),
-                            style: StrokeStyle(lineWidth: 1.0)
-                        )
-                )
-                .padding(.bottom, 10)
-                
-            
-            SecureField("Password", text: $viewModel.password)
-                .autocapitalization(.none)
-                .disableAutocorrection(false)
-                .padding()
-                .background(Color.white)
-                .cornerRadius(24.0)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 24.0)
-                        .strokeBorder(
-                            Color(UIColor.separator),
-                            style: StrokeStyle(lineWidth: 1.0)
-                        )
-                )
-                .padding(.bottom, 30)
-            
-            Button{
-                viewModel.signIn()
-            } label: {
-                Text("Login")
-                    .frame(maxWidth: .infinity)
+        NavigationView {
+            VStack {
+                Image("chat_logo")
+                    .resizable()
+                    .scaledToFit()
                     .padding()
-                    .background(Color("GreenColor"))
-                    .foregroundColor(Color.white)
+                
+                TextField("E-mail", text: $viewModel.email)
+                    .autocapitalization(.none)
+                    .disableAutocorrection(false)
+                    .padding()
+                    .background(Color.white)
                     .cornerRadius(24.0)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 24.0)
+                            .strokeBorder(
+                                Color(UIColor.separator),
+                                style: StrokeStyle(lineWidth: 1.0)
+                            )
+                    )
+                    .padding(.bottom, 10)
+                
+                
+                SecureField("Password", text: $viewModel.password)
+                    .autocapitalization(.none)
+                    .disableAutocorrection(false)
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(24.0)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 24.0)
+                            .strokeBorder(
+                                Color(UIColor.separator),
+                                style: StrokeStyle(lineWidth: 1.0)
+                            )
+                    )
+                    .padding(.bottom, 30)
+                
+                Button{
+                    viewModel.signIn()
+                } label: {
+                    Text("Login")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color("GreenColor"))
+                        .foregroundColor(Color.white)
+                        .cornerRadius(24.0)
                     
+                }
+                
+                Divider()
+                    .padding()
+                
+                NavigationLink(destination: SignUpView()){
+                    Text("Dont have an account? Click here")
+                        .foregroundColor(Color.black)
+                }
+                
             }
-            
-            Divider()
-                .padding()
-            
-            Button{
-                print("Clicked")
-            } label: {
-                Text("Dont have an account? Click here")
-                    .foregroundColor(Color.black)
-            }
-            
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding(.horizontal, 32)
+            .background(Color.init(red: 240 / 255, green: 231 / 255, blue: 210 / 255))
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .padding(.horizontal, 32)
-        .background(Color.init(red: 240 / 255, green: 231 / 255, blue: 210 / 255))
     }
 }
 
