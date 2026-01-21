fe08fbf4d2ca5997c2d5bbc9d754c8bba68ef33a
mateuslopes92 <mateuslopes92@gmail.com>
2026-01-21 09:37:39 -0300

    Created contact row view and async load profile image

--- SwiftApps/iChat/iChat/ContactsView.swift
+++ SwiftApps/iChat/iChat/ContactsView.swift
@@ -13,10 +13,27 @@ struct ContactsView: View {
     var body: some View {
         VStack {
             List(viewModel.contacts, id: \.self){contact in
-                Text("Contact name: \(contact.name)")
+                ContactRow(contact: contact)
             }
         }.onAppear {
             viewModel.fetchContacts()
+        }
+    }
+}
+
+struct ContactRow: View {
+    var contact: Contact
+    
+    var body: some View {
+        HStack {
+            AsyncImage(url: URL(string: contact.profileUrl)){image in
+                image.resizable().scaledToFit().clipShape(Circle())
+            } placeholder: {
+                ProgressView()
+            }
+            .frame(width: 50, height: 50)
+            
+            Text(contact.name)
         }
     }
 }
