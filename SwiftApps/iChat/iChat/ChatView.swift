d3889955bc91996e80e54e984c1b4f3830fd8d37
mateuslopes92 <mateuslopes92@gmail.com>
2026-03-10 23:08:58 -0300

    Ichat: added chatview with messages separated from senders

--- SwiftApps/iChat/iChat/ChatView.swift
+++ SwiftApps/iChat/iChat/ChatView.swift
@@ -17,6 +17,34 @@ struct ChatView: View {
                     MessageRow(message: message)
                 }
             }
+            
+            Spacer()
+            
+            HStack{
+                TextField("Type your message", text: $viewModel.text)
+                    .autocapitalization(.none)
+                    .disableAutocorrection(true)
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(24.0)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 24.0)
+                            .strokeBorder(Color(UIColor.separator), style: StrokeStyle(lineWidth: 1.0))
+                    )
+                
+                Button{
+                    //
+                } label: {
+                    Text("Send")
+                        .padding()
+                        .background(Color("GreenColor"))
+                        .foregroundColor(Color.white)
+                        .cornerRadius(24.0)
+                }
+                .disabled(viewModel.text.isEmpty)
+                
+            }
+            .padding(8.0)
         }
     }
 }
@@ -27,6 +55,13 @@ struct MessageRow: View {
     
     var body: some View {
         Text(message.text)
+            .background(Color(white: 0.95))
+            .frame(maxWidth: .infinity, alignment: message.isMe ?  .leading : .trailing)
+            .lineLimit(nil)
+            .fixedSize(horizontal: false, vertical: true)
+            .padding(.leading, message.isMe ? 0 : 50)
+            .padding(.trailing, message.isMe ? 50 : 0)
+            .padding(.vertical, 4)
     }
 }
 
